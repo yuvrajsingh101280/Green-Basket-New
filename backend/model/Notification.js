76654fa70fd2646069777e5f17bf526ed09b82ce
yuvrajsingh101280 <singhyuvraj65035@gmail.com>
2025-08-07 10:31:41 +0530

    Add socket.io dependency and update notification schema formatting

--- backend/model/Notification.js
+++ backend/model/Notification.js
@@ -1,19 +1,28 @@
 import mongoose from "mongoose";
-const notificationSchema = new mongoose.Schema({
 
-    userId: {
-
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User"
-
+const notificationSchema = new mongoose.Schema(
+    {
+        userId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        message: {
+            type: String,
+            required: true,
+        },
+        type: {
+            type: String,
+            enum: ["order", "promo", "system", "info", "warning"],
+            default: "info",
+        },
+        read: {
+            type: Boolean,
+            default: false,
+        },
     },
-    message: String,
+    { timestamps: true }
+);
 
-
-
-
-
-
-}, { timestamps: true })
-const Notification = mongoose.model("Notification", notificationSchema)
-export default Notification
+const Notification = mongoose.model("Notification", notificationSchema);
+export default Notification;
