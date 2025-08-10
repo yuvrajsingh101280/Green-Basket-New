79e339d1fb38eb158d7f01afb1af6d7833dbb723
yuvrajsingh101280 <singhyuvraj65035@gmail.com>
2025-08-10 17:50:13 +0530

    sd

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
+  base: '/', // ensures assets load correctly on Vercel
   plugins: [
     tailwindcss(),
   ],
